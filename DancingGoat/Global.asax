6afbae7c84e24dfcb08d48573d41d85ad5bab57a
sathyairavan <sathyairavan@gmail.com>
2022-06-07 18:52:31 +0000

    first commit

---
+++ DancingGoat/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="DancingGoat.MvcApplication" Language="C#" %>
---
+++ DancingGoat/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DancingGoat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("1f46a4ec-6a91-4f78-8b2b-5286c41414d9")]
+
+[assembly: AssemblyProduct("DancingGoat")]
+[assembly: AssemblyCopyright("© 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("12.0.0.0")]
+[assembly: AssemblyFileVersion("12.0.7115.13953")]
+[assembly: AssemblyInformationalVersion("12.0.29")]
